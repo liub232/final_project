1066e4d48f1daf2445cbf6f7078e5b0329eefa36
annabeisch <banana5525@icloud.com>
2024-06-25 13:09:02 -0700

    Initial Commit

---
+++ final_project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final_project
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
